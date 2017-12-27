7b7a9dfe025a470eaa6a503a6425eb6c8f67aee7
Pete Benoit <peterbenoit@gmail.com>
2017-12-27 17:46:42 -0500

    Build updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var gulp = require( 'gulp' ),
     sass = require( 'gulp-sass' ),
     sourcemaps = require( 'gulp-sourcemaps' ),
     minify = require( 'gulp-minify' ),
-    watch = require( 'gulp-watch' );
+    jshint = require('jshint'),
+    livereload = require('gulp-livereload'),
+    watch = require( 'gulp-watch' ),
+    nodemon = require('gulp-nodemon');
 
 var COMPILE = {
     ALL: 'src/**',
@@ -16,7 +19,8 @@ var COMPILE = {
 gulp.task( 'render', function() {
     return gulp.src( COMPILE.SRC )
     .pipe( nunjucks.compile() )
-    .pipe( gulp.dest( COMPILE.DEST ) );
+    .pipe( gulp.dest( COMPILE.DEST ) )
+    .pipe( livereload() );
 } );
 
 gulp.task( 'sass', function() {
@@ -24,22 +28,34 @@ gulp.task( 'sass', function() {
     .pipe( sourcemaps.init() ) 
     .pipe( sass().on( 'error', sass.logError ) )
     .pipe( sourcemaps.write() )
-    .pipe( gulp.dest( COMPILE.DEST + '/css' ) );
+    .pipe( gulp.dest( COMPILE.DEST + '/css' ) )
+    .pipe( livereload() );
 } );
 
 gulp.task( 'minify', function() {
     return gulp.src( COMPILE.JS )
     .pipe( minify() ) 
-    .pipe( gulp.dest( COMPILE.DEST + '/js' ) );
+    .pipe( gulp.dest( COMPILE.DEST + '/js' ) )
+    .pipe( livereload() );
 } );
 
-gulp.task( 'stream', ['default'], function() {
+gulp.task( 'watch', ['default'], function() {
+    livereload.listen();
     gulp.watch( COMPILE.ALL, ['default'] );
 } );
 
 gulp.task( 'copy', function() {
-    gulp.src(['src/assets/**']).pipe(gulp.dest('dist/assets'));
+    gulp.src( ['src/assets/**'] )
+    .pipe( gulp.dest('dist/assets') );
 } );
 
+gulp.task('server',function(){  
+    nodemon({
+        'script': 'app.js',
+        'ignore': 'dist/js/*.js'
+    });
+});
+
+gulp.task( 'serve', ['server','watch'] ); 
 
 gulp.task( 'default', ['render', 'sass', 'minify', 'copy'] );
